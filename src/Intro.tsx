78023825ab7af35b0340940918d1cb91366aa958
Paul Malmsten <paulmalmsten@microsoft.com>
2021-10-12 18:21:16 +0000

    Clean up imports and function name for intro

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 
-export default function AddressForm() {
+export default function Intro() {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
