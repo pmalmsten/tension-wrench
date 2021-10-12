ea5638a9b10438fe116bcaad2381b111127fccd9
Paul Malmsten <paulmalmsten@microsoft.com>
2021-10-12 18:25:04 +0000

    Simplify app imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
 import Checkout from './Checkout';
 
 export default function App() {
