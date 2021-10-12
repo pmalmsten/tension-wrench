b6a6e8aea07681210a958115cdd036bf533f5369
Paul Malmsten <paulmalmsten@microsoft.com>
2021-10-12 18:18:05 +0000

    Remove unnecessary copyright

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,6 @@ import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 import Checkout from './Checkout';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
-
 export default function App() {
   return (
     <Checkout /> 
