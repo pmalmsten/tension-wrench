bab5e80b4c0744ab60c46f1f2c6e3de32b967e43
Paul Malmsten <paulmalmsten@microsoft.com>
2021-10-12 18:23:26 +0000

    Remove checkout title from stepper flow

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -77,9 +77,6 @@ export default function Checkout() {
       </AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography component="h1" variant="h4" align="center">
-            Checkout
-          </Typography>
           <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
             {steps.map((label) => (
               <Step key={label}>
