de47d9d495f2e9e1ab5433c249b10126273439f8
Paul Malmsten <paulmalmsten@microsoft.com>
2021-10-14 18:56:45 +0000

    Flesh out Information Disclosure section

--- src/DiscussionGuideSteps.tsx
+++ src/DiscussionGuideSteps.tsx
@@ -67,7 +67,46 @@ export default function generateSteps(components: string[], componentTraitsMap: 
             },
             {
                 label: `${component}: Information Disclosure`,
-                content: <Typography>An attacker might try to extract data they should not have from this component.</Typography>,
+                content: <Typography>
+                    An attacker might try to extract data they should not have access to from this component.
+                    <p>Examples include:
+                    <ul>
+                        <li>An attacker gains access to the datacenter where this component is housed and walks away with the underlying physical storage.</li>
+                        <li>An attacker gains access to this component by exploiting a vulnerability in the OS or other software and extracts confidential information stored on disk.</li>
+                        <li>An attacker gains access to this component by exploiting incorrect access control settings and extracts confidential information from this resource.</li>
+                    </ul></p>
+                    <ProTip>
+                        Consider using an access control mechanism to restrict acess to this resource. Some examples include operating system user
+                        permissions, network access control lists, or cloud provider access mangement tools. Only grant access to users and systems
+                        that need access, and grant as little access as possible.
+                    </ProTip>
+                    <ProTip>
+                        For particularly sensitive / high-value information stored at rest, consider whether additional defenses may be appropriate. For example:
+                        <ul>
+                            <li>
+                                When storing passwords: consider whether you can avoid storing passwords at all by using a third-party authentication provider instead 
+                                (like <a href="https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-overview"> Microsoft Identity Platform</a>). If you must
+                                store passwords, follow guidelines for doing so safely (like 
+                                <a href="https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html">OWASP's Password Cheat Sheet</a>)
+                            </li>
+                            <li>
+                                When storing other confidential information: consider encrypting the sensitive information before storing it, which would make it much more difficult
+                                for an attacker to make use of any data they gain acess to. Follow guidelines like 
+                                <a href="https://cheatsheetseries.owasp.org/cheatsheets/Cryptographic_Storage_Cheat_Sheet.html">OWASP's Cryptographic Storage cheat sheet</a>, and/or regulatory
+                                standards like PCI-DSS, HIPAA, or FedRAMP that might apply to your use case.
+                            </li>
+                        </ul>
+                    </ProTip>
+                    {componentTraitNamesSet.has(Traits.AzureResource.name) && <ProTip>
+                        You indicated that this is an Azure resource - Many Azure resources offer tools for controlling which other systems can access access data, such as VNet support, 
+                        data plane role-based access control (such as for Cosmos DB), IP address filtering, and others. Consider how you might leverage these to limit access to this
+                        resource to just those users or systems who need it.
+                        
+                        <p>Azure offers <a href="https://docs.microsoft.com/en-us/azure/security/fundamentals/encryption-overview">a varity of tools to help you encrypt data</a>, both
+                        'server side' (encryption at rest provided transparently by Azure resources for you) and 'client side' (encryption your application performs before storing
+                        data).</p>
+                    </ProTip>}
+                </Typography>,
             },
             {
                 label: `${component}: Denial of Service`,
