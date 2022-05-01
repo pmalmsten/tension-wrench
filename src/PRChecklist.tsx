e3c96dcedf22a729c40708452ba16eb10bae4c49
Paul Malmsten <pmalmsten@gmail.com>
2022-05-01 19:23:46 +0000

    PR checklist: Added initial error handling for failure to load schema or checklist data

--- src/PRChecklist.tsx
+++ src/PRChecklist.tsx
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Box, Checkbox, CircularProgress, Container, List, ListItem, Paper, Typography } from "@mui/material";
+import { Alert, AlertTitle, Box, Checkbox, CircularProgress, Container, List, ListItem, Paper, Typography } from "@mui/material";
 import Ajv, { ValidateFunction } from "ajv";
 import React, { useEffect, useState } from "react";
-import YAML from "yaml";
+import YAML, { YAMLParseError } from "yaml";
 import "./ChecklistTypes";
 import { QuestionData, TaskListData, TaskData, ChecklistData } from "./ChecklistTypes";
 
@@ -53,31 +53,118 @@ function resolveTasksFor(taskLists: {[key: string]: TaskListData}, taskListName:
     return result;
 }
 
+interface LoadError {
+    title: string
+    messages: string[]
+}
+
 export default function PRChecklist() {
+    const [loadErrorInfo, setLoadErrorInfo] = useState<LoadError | undefined>(undefined)
     const [checklistDataSchema, setChecklistDataSchema] = useState<ValidateFunction<ChecklistData> | undefined>(undefined)
     const [dataObj, setDataObj] = useState<ChecklistData | undefined>(undefined);
 
     useEffect(() => {
+        if (loadErrorInfo !== undefined) {
+            return;
+        }
+
         if (checklistDataSchema === undefined) {
             fetch("generated/ChecklistDataSchema.json")
-                .then(response => response.json()
-                    .then(json => {
-                        var compiled = ajv.compile<ChecklistData>(json)
-                        // Wrap with function so that useState setter doesn't immediately call the validation function
-                        // while attempting to populate the value to store
-                        setChecklistDataSchema(() => compiled)
-                    }))
+                .then(response => {
+                    if (!response.ok) {
+                        setLoadErrorInfo({
+                            title: "Unable to load checklist schema",
+                            messages: [
+                                response.statusText
+                            ]
+                        })
+                    } else {
+                        response.json()
+                            .then(json => {
+                                try {
+                                    var compiled = ajv.compile<ChecklistData>(json)
+                                    // Wrap with function so that useState setter doesn't immediately call the validation function
+                                    // while attempting to populate the value to store
+                                    setChecklistDataSchema(() => compiled)
+                                } catch (ex) {
+                                    if (ex instanceof Error) {
+                                        setLoadErrorInfo({
+                                            title: "Unable to load checklist schema",
+                                            messages: [
+                                                ex.message
+                                            ]
+                                        })
+                                    } else {
+                                        setLoadErrorInfo({
+                                            title: "Unable to load checklist schema",
+                                            messages: [
+                                                "Unrecognized exception thrown"
+                                            ]
+                                        })
+                                    }
+                                }
+                            })
+                            .catch(ex => {
+                                if (ex instanceof SyntaxError) {
+                                    setLoadErrorInfo({
+                                        title: "Unable to parse checklist data schema as JSON",
+                                        messages: [
+                                            ex.message
+                                        ]
+                                    })
+                                } else {
+                                    setLoadErrorInfo({
+                                        title: "Unable to parse checklist data schema as JSON",
+                                        messages: []
+                                    })
+                                }
+                            })
+                    }
+                })
         } else if (dataObj === undefined) {
             fetch("default-pr-checklist.yml")
-                .then(response => response.text()
-                    .then(data => {
-                        var parsed = YAML.parse(data)
-                        if (checklistDataSchema(parsed)) {
-                            setDataObj(parsed)
-                        } else {
-                            throw new Error(`Checklist data failed to validate: ${checklistDataSchema.errors?.map(err => err.message).join(", ")}`)
-                        }
-                    }))
+                .then(response => {
+                    if (!response.ok) {
+                        setLoadErrorInfo({
+                            title: "Unable to load checklist data",
+                            messages: [
+                                response.statusText
+                            ]
+                        })
+                    } else {
+                        response.text()
+                            .then(data => {
+                                try {
+                                    var parsed = YAML.parse(data)
+                                } catch (ex) {
+                                    if (ex instanceof YAMLParseError) {
+                                        setLoadErrorInfo({
+                                            title: "Unable to parse checklist data as YAML",
+                                            messages: [
+                                                ex.message
+                                            ]
+                                        })
+                                    } else {
+                                        setLoadErrorInfo({
+                                            title: "Unable to parse checklist data as YAML",
+                                            messages: []
+                                        })
+                                    }
+                                    return
+                                }
+
+
+                                if (checklistDataSchema(parsed)) {
+                                    setDataObj(parsed)
+                                } else {
+                                    setLoadErrorInfo({
+                                        title: "Loaded checklist data does not conform to expected schema",
+                                        messages: checklistDataSchema.errors?.map(err => `At '${err.instancePath}': ${err.message}`) ?? []
+                                    })
+                                }
+                            })
+                    }
+                })
         }
     })
 
@@ -106,10 +193,19 @@ export default function PRChecklist() {
     <React.Fragment>
         <Container component="main" maxWidth='md' sx={{ mb: 4 }}>
             <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-                {!isFullyLoaded && 
+                {!isFullyLoaded && loadErrorInfo === undefined &&
                     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                         <CircularProgress />
                     </Box>}
+                {!isFullyLoaded && loadErrorInfo !== undefined && 
+                    <Alert severity="error" >
+                        <AlertTitle>{loadErrorInfo.title}</AlertTitle>
+                        {loadErrorInfo.messages.length > 0 && 
+                            <List>
+                                {loadErrorInfo.messages.map(err => <ListItem key={err}>{err}</ListItem>)}
+                            </List>
+                        }
+                    </Alert>}
                 {isFullyLoaded && 
                     <React.Fragment>
                         <Typography variant="h6" gutterBottom>
